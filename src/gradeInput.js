3dedc29364e4de895d14934352b6aba4a1f40fa9
DamonHolden <damonh003@gmail.com>
2022-05-08 04:43:34 +0100

    added digit constraint to input fields and updated code snippet

--- src/gradeInput.js
+++ src/gradeInput.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
+
 const GradeInput = (props) => {
+    
+    const [val, setVal] = useState();
+    
     return (
     <label > 
         {props.label}
         <br/> 
-        <input type="number" id={props.id} name={props.id} />
+        <input 
+            value={val} 
+            pattern="[0-9]*" 
+            onChange={
+                (e) => setVal(
+                    (v) => (
+                        e.target.validity.valid ? e.target.value : v
+                    )
+                )
+            }
+            type="text" 
+            id={props.id} 
+            name={props.id} 
+            maxLength={2} />
         <br/>
     </label>
     );
