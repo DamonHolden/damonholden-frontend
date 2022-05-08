ec62397de8c85b0689ef874ee165f518237c413b
DamonHolden <damonh003@gmail.com>
2022-05-08 02:40:36 +0100

    WAAITT, if you know you know

--- src/App.js
+++ src/App.js
@@ -13,48 +13,49 @@ function App() {
 
   return (
     <div className='App'>
-      <div className='content'>
-        <h1>UOS DTS Grade Calculator</h1>
-        <div className='card'>
-          <div className="row">
-            <div className="column">
-              <div>
-              <h2>Level 5</h2>
-              <form>
-                <GradeInput label="Introduction to Relational Databases: Assignment 1" id="introductionToRelationalDatabases1"/>
-                <GradeInput label="Introduction to Relational Databases: Assignment 2" id="introductionToRelationalDatabases2"/>
-                <GradeInput label="Advanced Networking Concepts" id="advancedNetworkingConcepts"/>
-                <GradeInput label="Data Structures Algorithms and Advanced Programming" id="dataStructuresAlgorithmsAndAdvancedProgramming"/>
-                <GradeInput label="Research Skills" id="researchSkills"/>
-                <GradeInput label="Software DesignDevelopment and Engineering: Assignment 1" id="softwareDesignDevelopmentAndEngineering1"/>
-                <GradeInput label="Software Design Development and Engineering: Assignment 2" id="softwareDesignDevelopmentAndEngineering2"/>
-                <GradeInput label="Multimedia Mobile and Internet" id="multimediaMobileAndInternet"/>
-              </form>
-              </div>
-            </div>
+      <div className='background' />
+        <div className='content'>
+          <h1>UOS DTS Grade Calculator</h1>
+          <div className='card'>
+            <div className="row">
               <div className="column">
                 <div>
-                <h2>Level 6</h2>
-                  <form>
-                    <GradeInput label="Distributed Systems" id="distributedSystems"/>
-                    <GradeInput label="Information Engineering" id="informationEngineering"/>
-                    <GradeInput label="Cyber Security Attack" id="cyberSecurityAttack"/>
-                    <GradeInput label="Cyber Security Defence" id="cyberSecurityDefence"/>
-                    <GradeInput label="Emergent Technologies" id="emergentTechnologies"/>
-                    <GradeInput label="Synoptic Project" id="synopticProject"/>
-                  </form>
+                <h2>Level 5</h2>
+                <form>
+                  <GradeInput label="Introduction to Relational Databases: Assignment 1" id="introductionToRelationalDatabases1"/>
+                  <GradeInput label="Introduction to Relational Databases: Assignment 2" id="introductionToRelationalDatabases2"/>
+                  <GradeInput label="Advanced Networking Concepts" id="advancedNetworkingConcepts"/>
+                  <GradeInput label="Data Structures Algorithms and Advanced Programming" id="dataStructuresAlgorithmsAndAdvancedProgramming"/>
+                  <GradeInput label="Research Skills" id="researchSkills"/>
+                  <GradeInput label="Software DesignDevelopment and Engineering: Assignment 1" id="softwareDesignDevelopmentAndEngineering1"/>
+                  <GradeInput label="Software Design Development and Engineering: Assignment 2" id="softwareDesignDevelopmentAndEngineering2"/>
+                  <GradeInput label="Multimedia Mobile and Internet" id="multimediaMobileAndInternet"/>
+                </form>
                 </div>
-                  <div className="calculateLocation">
-                    <input className="calculate" type="button" value="calculate" onClick={() => setResult(degreeCalculator())}/>
-                  </div>
               </div>
+                <div className="column">
+                  <div>
+                  <h2>Level 6</h2>
+                    <form>
+                      <GradeInput label="Distributed Systems" id="distributedSystems"/>
+                      <GradeInput label="Information Engineering" id="informationEngineering"/>
+                      <GradeInput label="Cyber Security Attack" id="cyberSecurityAttack"/>
+                      <GradeInput label="Cyber Security Defence" id="cyberSecurityDefence"/>
+                      <GradeInput label="Emergent Technologies" id="emergentTechnologies"/>
+                      <GradeInput label="Synoptic Project" id="synopticProject"/>
+                    </form>
+                  </div>
+                    <div className="calculateLocation">
+                      <input className="calculate" type="button" value="calculate" onClick={() => setResult(degreeCalculator())}/>
+                    </div>
+                </div>
+            </div>
+            <Result degree={result}/>
+            {/* <div className="output" id="output">{result}</div> */}
           </div>
-          <Result degree={result}/>
-          {/* <div className="output" id="output">{result}</div> */}
+          <CodeSnippet />
+          <Footer />
         </div>
-        <CodeSnippet />
-        <Footer />
-      </div>
     </div>
   );
 }
