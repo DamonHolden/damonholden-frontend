30c8770317243ebdd61353eac03851dc7f7f092f
DamonHolden <damonh003@gmail.com>
2022-05-08 13:28:54 +0100

    cleaned up calculator spacing

--- src/App.js
+++ src/App.js
@@ -19,23 +19,23 @@ function App() {
           <div className='card'>
             <div className="row">
               <div className="column">
-                <div>
-                <h2>Level 5</h2>
-                <form>
-                  <GradeInput label="Introduction to Relational Databases: Assignment 1" id="introductionToRelationalDatabases1"/>
-                  <GradeInput label="Introduction to Relational Databases: Assignment 2" id="introductionToRelationalDatabases2"/>
-                  <GradeInput label="Advanced Networking Concepts" id="advancedNetworkingConcepts"/>
-                  <GradeInput label="Data Structures Algorithms and Advanced Programming" id="dataStructuresAlgorithmsAndAdvancedProgramming"/>
-                  <GradeInput label="Research Skills" id="researchSkills"/>
-                  <GradeInput label="Software DesignDevelopment and Engineering: Assignment 1" id="softwareDesignDevelopmentAndEngineering1"/>
-                  <GradeInput label="Software Design Development and Engineering: Assignment 2" id="softwareDesignDevelopmentAndEngineering2"/>
-                  <GradeInput label="Multimedia Mobile and Internet" id="multimediaMobileAndInternet"/>
-                </form>
+                <div className='form'>
+                  <h2>Level 5</h2>
+                  <form>
+                    <GradeInput label="Introduction to Relational Databases: Assignment 1" id="introductionToRelationalDatabases1"/>
+                    <GradeInput label="Introduction to Relational Databases: Assignment 2" id="introductionToRelationalDatabases2"/>
+                    <GradeInput label="Advanced Networking Concepts" id="advancedNetworkingConcepts"/>
+                    <GradeInput label="Data Structures Algorithms and Advanced Programming" id="dataStructuresAlgorithmsAndAdvancedProgramming"/>
+                    <GradeInput label="Research Skills" id="researchSkills"/>
+                    <GradeInput label="Software DesignDevelopment and Engineering: Assignment 1" id="softwareDesignDevelopmentAndEngineering1"/>
+                    <GradeInput label="Software Design Development and Engineering: Assignment 2" id="softwareDesignDevelopmentAndEngineering2"/>
+                    <GradeInput label="Multimedia Mobile and Internet" id="multimediaMobileAndInternet"/>
+                  </form>
                 </div>
               </div>
                 <div className="column">
-                  <div>
-                  <h2>Level 6</h2>
+                  <div className='form'>
+                    <h2>Level 6</h2>
                     <form>
                       <GradeInput label="Distributed Systems" id="distributedSystems"/>
                       <GradeInput label="Information Engineering" id="informationEngineering"/>
@@ -45,17 +45,16 @@ function App() {
                       <GradeInput label="Synoptic Project" id="synopticProject"/>
                     </form>
                   </div>
-                    <div className="calculateLocation">
-                      <input className="calculate" type="button" value="calculate" onClick={() => setResult(degreeCalculator())}/>
-                    </div>
+                  <div className="calculateLocation">
+                    <input className="calculate" type="button" value="calculate" onClick={() => setResult(degreeCalculator())}/>
+                  </div>
                 </div>
-            </div>
+              </div>
             <Result degree={result}/>
-            {/* <div className="output" id="output">{result}</div> */}
           </div>
-          <CodeSnippet />
-          <Footer />
-        </div>
+        <CodeSnippet />
+        <Footer />
+      </div>
     </div>
   );
 }
