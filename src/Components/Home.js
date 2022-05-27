0cad0c12e6b6803c27661a2bc94e991ad1125c3c
DamonHolden <damonh003@gmail.com>
2022-05-27 15:27:24 +0100

    fix: Add github logo

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,58 +3,60 @@ import { Card } from './Card';
 
 const Home = () => {
   const eddieHearnLink =
-    ' some positive words from boxing promoter Eddie Hearn.';
+    ' some positive words, from boxing promoter, Eddie Hearn.';
 
   return (
     <>
-      <h1>hello, one and all</h1>
+      <h1>Hi, I'm Damon, and on occasion, I like to build things.</h1>
       <Card>
         <h2>So what is this site for?</h2>
-        <p>
-          This website showcases various bits and bobs that I (Damon Holden)
-          develop in my spare time.
-        </p>
-        <p>
-          Links to all the various pages on this site can be found at the top of
-          this page and my contact email and GitHub repository links can be
-          found at the bottom of the page.
-        </p>
-        <p>
-          This sites architecture is based on the{' '}
-          <a
-            className='eddie-hearn-link'
-            href='https://www.mongodb.com/mern-stack'
-            target={'_blank'}
-            rel='noreferrer'
-          >
-            MERN
-          </a>
-          stack, however the project is only really a React application with a
-          very basic Express Node.js application setup as a template backend
-          which will eventually link to a cloud-based mongoDB database. The
-          React frontend is hosted by Netlify, and the Express Node.js backend
-          server is hosted by Heroku. Currently, mongoDB is deployed through
-          their Atlas service on an AWS cloud cluster, the functionality of
-          which is just the link between the database and the backend with no
-          actual practical functionality just yet.
-        </p>
-        <p>
-          a representation of this deployment setup is laid-out at a high-level
-          in the deployment diagram, below:
-        </p>
-        <img alt='deployment diagram' src={deploymentDiagram} />
-        <p>
-          If you found nothing useful from any of the projects on this site,
-          here are
-          <a
-            className='eddie-hearn-link'
-            href='https://m.youtube.com/watch?t=1505&v=HXpj7mpm6hA&feature=youtu.be'
-            target={'_blank'}
-            rel='noreferrer'
-          >
-            {eddieHearnLink}
-          </a>
-        </p>
+        <div className='text-box'>
+          <p>
+            This website showcases various bits'n'bobs that I develop in my
+            spare time.
+          </p>
+          <p>
+            Links to different pages within this site can be found at the top of
+            this page and my contact, github and whatnot can be found at the
+            bottom of the page.
+          </p>
+          <p>
+            This sites architecture is based on the{' '}
+            <a
+              className='eddie-hearn-link'
+              href='https://www.mongodb.com/mern-stack'
+              target={'_blank'}
+              rel='noreferrer'
+            >
+              MERN
+            </a>
+            stack, however the project is only really a React application with a
+            very basic Express Node.js application setup as a template backend
+            which will eventually link to a cloud-based mongoDB database. The
+            React frontend is hosted by Netlify, and the Express Node.js backend
+            server is hosted by Heroku. Currently, mongoDB is deployed through
+            their Atlas service on an AWS cloud cluster, the functionality of
+            which is just the link between the database and the backend with no
+            actual practical functionality just yet.
+          </p>
+          <p>
+            A representation of this deployment setup is laid-out at a
+            high-level in the deployment diagram, below:
+          </p>
+          <img alt='deployment diagram' src={deploymentDiagram} />
+          <p>
+            You should always try and get something positive out of any
+            experience, so if nothing else here are
+            <a
+              className='eddie-hearn-link'
+              href='https://m.youtube.com/watch?t=1505&v=HXpj7mpm6hA&feature=youtu.be'
+              target={'_blank'}
+              rel='noreferrer'
+            >
+              {eddieHearnLink}
+            </a>
+          </p>
+        </div>
       </Card>
     </>
   );
