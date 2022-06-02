2b8f0aff09bdb90ff43ae072d13b553dd1544b64
DamonHolden <damonh003@gmail.com>
2022-06-02 21:18:02 +0100

    feat(databasetester): add pokemon search

--- src/Components/DatabaseTester.js
+++ src/Components/DatabaseTester.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Card } from './Card';
 import axios from 'axios';
 import { animated, useSpring } from 'react-spring';
@@ -9,20 +9,37 @@ export const DatabaseTester = () => {
     from: { opacity: 0 },
   });
 
+  const searchName = useRef();
+
   const [response, setResponse] = useState(
     'No response from backend this time'
   );
-
-  axios
-    .get('https://damonholden-backend.herokuapp.com/endpoint')
-    .then((response) => {
-      setResponse(response.data);
-    });
+  const [hasSearched, setHasSearched] = useState(false);
 
   return (
     <animated.div className='page' style={springProps}>
       <h1>Database Tester</h1>
       <Card>
+        <input ref={searchName} type='text' />
+        <button
+          className='button-primary'
+          onClick={() => {
+            setHasSearched(true);
+            axios
+              .get(`http://localhost:3001/pokemon/${searchName.current.value}`)
+              .then((response) => {
+                setResponse(response.data);
+              })
+              .catch((error) => {
+                console.log(error);
+                setHasSearched(false);
+              });
+          }}
+        >
+          Search
+        </button>
+      </Card>
+      {/* <Card>
         <h2>Wow, such empty</h2>
         <p>
           This section of the site is a placeholder for an eventual way to
@@ -33,13 +50,15 @@ export const DatabaseTester = () => {
           section of the site will allow users to produce API requests to a
           backend.
         </p>
-      </Card>
-      <Card>
-        <h2>{response}</h2>
-        {response === 'Hello from the Backend!!!' ? (
-          <p>provided by express</p>
-        ) : null}
-      </Card>
+      </Card> */}
+      {hasSearched && (
+        <Card>
+          <h2>Name: {response.name}</h2>
+          <h2>Type: {response.type}</h2>
+          <h2>pokedex number: {response.pokedex_number}</h2>
+          {response ? <p>provided by express</p> : null}
+        </Card>
+      )}
     </animated.div>
   );
 };
