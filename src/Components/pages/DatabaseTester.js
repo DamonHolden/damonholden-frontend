339cc311837f192d57bbe71fe928be1ac30fb242
DamonHolden <damonh003@gmail.com>
2022-06-04 05:20:05 +0100

    feat: add pokemon search

--- src/Components/pages/DatabaseTester.js
+++ src/Components/pages/DatabaseTester.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Card } from '../Card';
 import axios from 'axios';
 import { PageWrapper } from '../PageWrapper';
@@ -8,32 +8,58 @@ export const DatabaseTester = () => {
     'No response from backend this time'
   );
 
-  axios
-    .get('https://damonholden-backend.herokuapp.com/endpoint')
-    .then((response) => {
-      setResponse(response.data);
-    });
+  const searchName = useRef();
+
+  const searchForPokemon = () => {
+    axios
+      .get(
+        `https://damonholden-backend-2.herokuapp.com/pokemon/${searchName.current.value}`
+      )
+      .then((response) => {
+        if (response.data === null) return;
+        else setResponse(response.data);
+      });
+  };
 
   return (
     <PageWrapper>
-      <h1>Database Tester</h1>
+      <h1>pokemon search</h1>
       <Card>
-        <h2>Wow, such empty</h2>
-        <p>
-          This section of the site is a placeholder for an eventual way to
-          facilitate backend database communication.
-        </p>
-        <p>
-          Currently, there is no database set-up at all, but eventually, this
-          section of the site will allow users to produce API requests to a
-          backend.
-        </p>
+        <div>
+          <input
+            ref={searchName}
+            type='text'
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') searchForPokemon();
+            }}
+          />
+          <button className='button-primary' onClick={() => searchForPokemon()}>
+            Search
+          </button>
+        </div>
       </Card>
       <Card>
-        <h2>{response}</h2>
-        {response === 'Hello from the Backend!!!' ? (
-          <p>provided by express</p>
-        ) : null}
+        <div
+          className='grade-form-area'
+          style={{
+            alignItems: 'center',
+          }}
+        >
+          <img
+            style={{ width: '200px' }}
+            src={
+              response.name
+                ? require(`../../images/pokemon/${response.name}.png`)
+                : null
+            }
+            alt='pokemon'
+          />
+          <div>
+            <h2>Name: {response.name}</h2>
+            <h2>Type: {response.type}</h2>
+            <h2>pokedex number: {response.pokedex_number}</h2>
+          </div>
+        </div>
       </Card>
     </PageWrapper>
   );
